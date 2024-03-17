d8fa14fa0c29e519bd4438878070f778ae5075ce
Rishikulborah123 <rishikulborah@gmail.com>
2024-03-18 00:30:12 +0530

    made changes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let nav=document.getElementsByTagName("li")
-let about=document.getElementsByClassName("about")
-nav[1].addEventListener("click",()=>{
-    about.textContent=about;
-})
+let sidemenu=document.getElementById("sidemenu")
+function openmenu(){
+    sidemenu.style.right="-230px";
+}
+
+function closemenu(){
+    sidemenu.style.right="-470px";
+}
